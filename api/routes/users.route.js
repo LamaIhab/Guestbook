4730bc9394bdca2b01b538dc7f7af769c6d8acc3
LamaIhab <lamloma19@hotmail.com>
2020-05-11 00:06:08 +0200

    added route for login in users.route

--- api/routes/users.route.js
+++ api/routes/users.route.js
@@ -4,5 +4,6 @@ const users = require('../controllers/userControllers')
 const router = express.Router()
 
 router.post('/signUp',users.signUp)
+router.post('/logIn',users.logIn)
 
 module.exports = router
