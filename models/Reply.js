ee00e9f2ac53ec3171b08c3b273801be8b32fe96
LamaIhab <lamloma19@hotmail.com>
2020-05-11 23:18:52 +0200

    created model for replies on messages

---
+++ models/Reply.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const replySchema = new Schema({
+    username:{
+        type:'String',
+        required:'true'
+    },
+    description:{
+        type:'String',
+        required:'true'
+
+    },
+    date:{
+        type:Date,
+        required:false,
+        default:Date()
+    },
+    messageID:{
+        type:'String',
+        required:true  // will be handled in front end more
+    }
+  
+})
+module.exports = Reply = mongoose.model(replies,replySchema)
