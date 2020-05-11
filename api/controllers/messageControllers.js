6044a25c1b406e6bac0f0e2c7139978c0943292c
LamaIhab <lamloma19@hotmail.com>
2020-05-11 17:52:21 +0200

    updated schema of message to only take username of user logged in without refrencing the whole object for simplicity

---
+++ api/controllers/messageControllers.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Message = require('./models/Message')
+
+// create a new message
+
+const postMessage = async(req,res)=>{
+    try{
+        
+
+    }
+    catch(error){
+        console.error(error)
+    }
+
+}
--- models/Message.js
+++ models/Message.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const messageSchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required:true
+    username: {
+        type: 'String',
+        required:'true'
+     
        
       }, // signed in user can only post a message
       description:{
