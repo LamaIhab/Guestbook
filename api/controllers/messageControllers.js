388c3547cf2dee8d8373f8b434053a10db68c284
LamaIhab <lamloma19@hotmail.com>
2020-05-11 22:04:14 +0200

    created function to edit a message

--- api/controllers/messageControllers.js
+++ api/controllers/messageControllers.js
@@ -22,6 +22,35 @@ const postMessage = async(req,res)=>{
     }
 
 }
+
+// edit message
+const editMessage = async(req,res)=>{
+
+    try{
+        message = await Message.findById(req.params.id)
+        if(!message)
+       return res.json({msg:'this message does not exist'})
+       else{
+           const user = req.body.username // get username of signed in user to access editing of message
+           if(message.usename!==user)
+           return res.json({msg:'you can only edit your own message'})
+           else{
+               const updatedMessage = await Message.findOneAndUpdate(req.params.id,req.body)
+               return res.json({msg:'Message updated successfully',data:updatedMessage})
+           }
+
+       }
+
+    }
+    catch(error){
+        console.error(error)
+    }
+}
+
+// get all messages
+
+
+// delete a message
 module.exports={
     postMessage
 }
